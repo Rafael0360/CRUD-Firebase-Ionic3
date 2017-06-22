a3a657ca3178f3dc92f7ae5f2255ae5cc3cd4224
Gustavokmp <gustavomp.ti@gmail.com>
2017-06-22 15:39:57 -0300

    Integrated with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,21 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//import angularfire
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+
+var config = {
+    apiKey: "AIzaSyBHrYKd3govWRmTRSj4OgWNhq4maR5KI9o",
+    authDomain: "angularfire-crud-1435b.firebaseapp.com",
+    databaseURL: "https://angularfire-crud-1435b.firebaseio.com",
+    projectId: "angularfire-crud-1435b",
+    storageBucket: "angularfire-crud-1435b.appspot.com",
+    messagingSenderId: "293211702518"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -18,6 +33,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    //import angularfire
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
